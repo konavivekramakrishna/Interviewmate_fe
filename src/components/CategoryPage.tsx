29bd93dcb8c5acbc5e01dabfc7fec0af4ca79af5
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-12-25 17:01:04 +0530

    integrated open-ai API in application

--- src/components/CategoryPage.tsx
+++ src/components/CategoryPage.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import AudioInput from "./AudioInput";
+import OpenAI from "openai";
 
-const CategoryPage = ({name}: {name:string}) => {
+const CategoryPage = ({ name }: { name: string }) => {
+  // const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
+  // console.log(apiKey);
+  // const openai = new OpenAI({
+  //   apiKey: apiKey,
+  //   dangerouslyAllowBrowser: true,
+  // });
+
+  // async function main() {
+  //   const completion = await openai.chat.completions.create({
+  //     messages: [{ role: "system", content: "What is your name." }],
+  //     model: "gpt-3.5-turbo",
+  //   });
+
+  //   console.log(completion.choices[0].message.content);
+  // }
+
 
   return (
-    <div className='mt-14'>
+    <div className="mt-14">
       <h2>Category Page: {name}</h2>
 
-      {/* Other content related to the category page */}
+      {/* <button onClick={() => main()}>run</button> */}
+      <AudioInput />
     </div>
   );
 };
