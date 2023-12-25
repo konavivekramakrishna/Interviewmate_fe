fb0ee38159ed04146b9938604afed4b5ed021aae
vivek <konavivekramakrishna@gmail.com>
2023-12-25 15:47:10 +0530

    added Audioinput component

---
+++ src/components/AudioInput.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import useClipboard from "react-use-clipboard";
+
+const AudioInput = () => {
+  const [textToCopy, setTextToCopy] = React.useState("");
+  const [isCopied, setCopied] = useClipboard(textToCopy, {
+    successDuration: 1000,
+  });
+
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  const printTranscript = () => {
+    console.log(transcript);
+  };
+
+  if (!browserSupportsSpeechRecognition) {
+    return <p>Your browser does not support speech recognition.</p>;
+  }
+
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+
+  return (
+    <>
+      <div className="container">
+        <h2>Speech to Text Converter</h2>
+        <br />
+        <p>
+          A React hook that converts speech from the microphone to text and
+          makes it available to your React components.
+        </p>
+        <div
+          className="main-content"
+          onClick={() => {
+            setTextToCopy(transcript);
+          }}
+        >
+          {transcript}
+        </div>
+        <div className="btn-style">
+          <button onClick={setCopied}>
+            {isCopied ? "Copied!" : "Copy to clipboard"}
+          </button>
+          <button className="bg-blue-500 m-4" onClick={startListening}>
+            Start Listening
+          </button>
+          <button
+            className="bg-red-500 m-4"
+            onClick={SpeechRecognition.stopListening}
+          >
+            Stop Listening
+          </button>
+          <button className="bg-green-500 m-4" onClick={printTranscript}>
+            Print Transcript
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AudioInput;
