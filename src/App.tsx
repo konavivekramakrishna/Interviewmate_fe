ad80083bed97f8ad348c7998b004f45f6e2fe350
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-12-25 12:06:43 +0530

    add navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 
 import "./App.css";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { ActiveLink } from "raviger";
+
+export const NavBar = () => {
+  return (
+    <div className="flex flex-col bg-slate-700 p-2 mr-2 justify-between w-full fixed">
+      <div className="flex gap-2 items-center text-white">
+        <h2 className="flex items-center p-2 mx-2 rounded-lg font-extrabold">
+          Interview Mate
+        </h2>
+        <div className="flex flex-row">
+          <ActiveLink
+            href="/"
+            className="flex items-center justify-center p-2 mx-1 text-gray-900 rounded-lg dark:text-white dark:hover:bg-slate-900 group"
+            exactActiveClass="border border-slate-200"
+          >
+            <span className="flex-1 whitespace-nowrap">Home</span>
+          </ActiveLink>
+          <ActiveLink
+            href="/about"
+            className="flex items-center p-2 mx-1 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-slate-900 group"
+            exactActiveClass="border border-slate-200"
+          >
+            <span className="flex-1 whitespace-nowrap">About</span>
+          </ActiveLink>
+        </div>
+      </div>
+    </div>
+  );
+};
